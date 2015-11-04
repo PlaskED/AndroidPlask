a175356ac8d536c3dcae7510c3fe889325f24c94
oscho707 <oscho707@student.liu.se>
2015-11-04 13:30:51 +0100

    skapat appen

---
+++ app/src/main/java/se/liu/oscho707student/plaskapp/MainActivity.java
@@ -0,0 +1,21 @@
+package se.liu.oscho707student.plaskapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fm_t = fm.beginTransaction();
+        //fm_t.add(R.id.mainView, new ListFragment()); //huvudmenyfragment
+        //fm_t.commit();
+
+    }
+
+}
